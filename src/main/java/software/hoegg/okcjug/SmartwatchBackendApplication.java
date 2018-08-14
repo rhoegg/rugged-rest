6ca9cf49fd952ba3fcbfbff899a51e86d25f6b54
Ryan Hoegg <ryan.hoegg@gmail.com>
2018-08-14 04:56:06 -0500

    Spring initializr generated

---
+++ src/main/java/software/hoegg/okcjug/SmartwatchBackendApplication.java
@@ -0,0 +1,11 @@
+package software.hoegg.okcjug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class SmartwatchBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartwatchBackendApplication.class, args);
+	}
+}
